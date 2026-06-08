34ddbefc2258af4b713e8629a1173dafd577f59a
abduaslam <abduaslam098@gmail.com>
2026-06-08 18:46:08 +0200

    Books scraping

--- EbooksScraper/EbooksScraper/spiders/BooksSpider1.py
+++ EbooksScraper/EbooksScraper/spiders/BooksSpider1.py
@@ -1,10 +1,21 @@
 import scrapy
-
-class BookSpider(scrapy.spider):
+# Create spider
+class BookSpider(scrapy.Spider):
     name="books"
     # send request
     start_urls=["https://books.toscrape.com/"]
     # get Response
     def parse(self,response):
         print("This our response")
-        print(response)
+        # use css selector
+        print(response.css("h3 a::text").get())
+        # Select all books
+        print ("all books info")
+        ebooks=response.css("article")
+        for book in ebooks :
+            title=book.css("a::text").get()
+            print(title)
+            price=book.css("p.price_color::text").get()
+            print(price)
+            availability=book.css("p.instock availability::text").get()
+            print(availability)
