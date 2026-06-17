9dbb35522c37192df6be4eb5ad5d5c872248b216
abduaslam <abduaslam098@gmail.com>
2026-06-17 18:45:15 +0200

    Books scraping

--- EbooksScraper/EbooksScraper/spiders/BooksSpider1.py
+++ EbooksScraper/EbooksScraper/spiders/BooksSpider1.py
@@ -54,9 +54,9 @@ class BookSpider(scrapy.Spider):
         for book in ebooks :
             loader=ItemLoader(item=bookitem())
             loader.add_value('title',book.css("a::text").get())
-            loader.add_value('price',Price_Int(book.css("p.price_color::text").get()))
+            loader.add_value('price',(book.css("p.price_color::text").get()))
             loader.add_value('availability',book.css("p.instock.availability").xpath("normalize-space()").get())
-            yield bookitem ;
+            yield loader.load_item() ;
 
            
       
