f26a04352ca656d90babb01e72a92335b0f19a1c
abduaslam <abduaslam098@gmail.com>
2026-06-18 14:09:11 +0200

    Books scraping

--- EbooksScraper/EbooksScraper/spiders/BooksSpider1.py
+++ EbooksScraper/EbooksScraper/spiders/BooksSpider1.py
@@ -25,34 +25,34 @@ class BookSpider(scrapy.Spider):
         # Select all books
         print ("all books info")
         ebooks=response.css("article")
-        for book in ebooks :
-            title=book.css("a::text").get()
-            print(title)
-            price=book.css("p.price_color::text").get()
-            availability=book.css("p.instock.availability").xpath("normalize-space()").get()
-            yield {
-                "title":title,
-                "Price":price ,
-                 "availability" :availability          }
+        # for book in ebooks :
+        #     title=book.css("a::text").get()
+        #     print(title)
+        #     price=book.css("p.price_color::text").get()
+        #     availability=book.css("p.instock.availability").xpath("normalize-space()").get()
+        #     yield {
+        #         "title":title,
+        #         "Price":price ,
+        #          "availability" :availability          }
         # #######################################################################
 
         # create object from our Items
         bookitem=booksItem()
-        print(["Thes Scrapy Items "])
-        # Select all books
-        print ("all books info")
-        ebooks=response.css("article")
-        for book in ebooks :
-            bookitem['title']=book.css("a::text").get()
-            bookitem['price']=Price_Int(book.css("p.price_color::text").get())
-            print(bookitem['price'])
-            bookitem['availability']=book.css("p.instock.availability").xpath("normalize-space()").get()
-            yield bookitem ;
+        # print(["Thes Scrapy Items "])
+        # # Select all books
+        # print ("all books info")
+        # ebooks=response.css("article")
+        # for book in ebooks :
+        #     bookitem['title']=book.css("a::text").get()
+        #     bookitem['price']=Price_Int(book.css("p.price_color::text").get())
+        #     print(bookitem['price'])
+        #     bookitem['availability']=book.css("p.instock.availability").xpath("normalize-space()").get()
+        #     yield bookitem ;
             # #####################################################################
             #Loading Items with Scrapy ItemLoaders
         print("[Loading Items with Scrapy ItemLoaders]")   
         for book in ebooks :
-            loader=ItemLoader(item=bookitem())
+            loader=ItemLoader(item=bookitem)
             loader.add_value('title',book.css("a::text").get())
             loader.add_value('price',(book.css("p.price_color::text").get()))
             loader.add_value('availability',book.css("p.instock.availability").xpath("normalize-space()").get())
